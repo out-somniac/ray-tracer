ddeff09d5826602fc9b6711065ea53b2e7c2bf10
Krzysztof Pęczek <peczek.kr@gmail.com>
2023-12-11 15:11:05 +0100

    Implemented `PartialOrd` for `HitRecord`

--- src/hits.rs
+++ src/hits.rs
@@ -1,10 +1,24 @@
 use crate::ray::Ray;
 use cgmath::Vector3;
+use std::cmp::Ordering;
 
 pub struct HitRecord {
     pub hit: Vector3<f64>,
     pub distance: f64,
     pub normal: Vector3<f64>
+}
+
+
+impl PartialOrd for HitRecord {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.distance.partial_cmp(&other.distance)
+    }
+}
+
+impl PartialEq for HitRecord {
+    fn eq(&self, other: &Self) -> bool {
+        self.distance == other.distance
+    }
 }
 
 pub trait Hittable {
